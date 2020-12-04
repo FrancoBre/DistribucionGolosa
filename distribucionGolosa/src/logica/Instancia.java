dd88bc016f155ddfdc33087cb568229d1a274fc6
LuisBregoli <LuisBregoli@172.16.5.90>
2020-12-04 20:22:48 -0300

    Se crearon la clase instancia y solver para resolver el algoritmo goloso
    Solver está vacío

---
+++ distribucionGolosa/src/logica/Instancia.java
@@ -0,0 +1,37 @@
+package logica;
+
+import java.util.ArrayList;
+
+public class Instancia {
+	ArrayList<Cliente> clientes;
+	ArrayList<CentroDistribucion> centros;
+	int k;
+	
+	public Instancia(ArrayList<Cliente> clientes, ArrayList<CentroDistribucion> centros, int k) {
+		this.clientes = clientes;
+		this.centros = centros;
+		this.k = k;
+	}
+	
+	public ArrayList<Cliente> getClientes() {
+		return clientes;
+	}
+	public void setClientes(ArrayList<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public ArrayList<CentroDistribucion> getCentros() {
+		return (ArrayList<CentroDistribucion>) centros.clone();
+	}
+
+	public void setCentros(ArrayList<CentroDistribucion> centros) {
+		this.centros = centros;
+	}
+	public int getK() {
+		return k;
+	}
+	public void setK(int k) {
+		this.k = k;
+	}
+}
---
+++ distribucionGolosa/src/logica/Solver.java
@@ -0,0 +1,17 @@
+package logica;
+
+import java.util.ArrayList;
+
+public class Solver {
+	Instancia instancia;
+	
+	public Solver(Instancia instancia) {
+		this.instancia = instancia;
+	}
+	
+	public ArrayList<CentroDistribucion> resolver() {
+		// TO DO
+		
+		return null;
+	}
+}
