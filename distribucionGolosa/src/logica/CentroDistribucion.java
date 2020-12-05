90b4571e341991cc86af7f86f883c9b5bf931bde
FrancoBre <bregolif.fb@gmail.com>
2020-12-05 13:34:50 -0300

    Constructores con el nombre para centro de distribución y cliente

--- distribucionGolosa/src/logica/CentroDistribucion.java
+++ distribucionGolosa/src/logica/CentroDistribucion.java
@@ -1,10 +1,15 @@
 package logica;
 
 public class CentroDistribucion extends Nodo {
-
+	String nombre;
+	Coordenada coor;
+	
 	public CentroDistribucion(Coordenada coor) {
 		super(coor);
-
 	}
-
+	
+	public CentroDistribucion(Coordenada coor, String nombre) {
+		super(coor);
+		this.nombre = nombre;
+	}
 }
--- distribucionGolosa/src/logica/Cliente.java
+++ distribucionGolosa/src/logica/Cliente.java
@@ -1,10 +1,15 @@
 package logica;
 
 public class Cliente extends Nodo {
-
+	String nombre;
+	Coordenada coor;
+	
 	public Cliente(Coordenada coor) {
 		super(coor);
-
 	}
-
+	
+	public Cliente(Coordenada coor, String nombre) {
+		super(coor);
+		this.nombre = nombre;
+	}
 }
