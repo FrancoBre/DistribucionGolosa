ffce3378c4b4cadc544614c12006bd1c91b7bb92
Usuario <Usuario@DESKTOP-SJR6E1A>
2020-12-09 18:30:59 -0300

    Cambios minimos en interfaz

--- distribucionGolosa/src/interfaz/Interfaz.java
+++ distribucionGolosa/src/interfaz/Interfaz.java
@@ -10,6 +10,9 @@ import javax.swing.JTextArea;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Interfaz {
 
@@ -43,28 +46,65 @@ public class Interfaz {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(Color.GREEN);
+		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
 		frame.setAlwaysOnTop(true);
-		frame.setBounds(100, 100, 331, 300);
+		frame.setBounds(100, 100, 619, 275);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JButton btnNewButton = new JButton("Leer clientes");
-		btnNewButton.setBounds(61, 84, 175, 41);
+		//Action leer Clientes
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(10, 58, 175, 29);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Leer posibles distribuidoras");
-		btnNewButton_1.setBounds(61, 140, 175, 41);
+		//Action leer Distribuidoras
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_1.setBounds(10, 92, 175, 29);
 		frame.getContentPane().add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("Mostrar soluci\u00F3n");
-		btnNewButton_2.setBounds(61, 192, 175, 41);
+		JButton btnNewButton_2 = new JButton("Solución 1");
+		//Action Solución 1
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_2.setBounds(10, 126, 175, 29);
 		frame.getContentPane().add(btnNewButton_2);
 		
 		JLabel lblNewLabel = new JLabel("Distribuci\u00F3n Golosa");
-		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblNewLabel.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 18));
 		lblNewLabel.setToolTipText("Distribuci\u00F3n Golosa");
-		lblNewLabel.setBounds(73, 23, 210, 36);
+		lblNewLabel.setBounds(10, 11, 182, 36);
 		frame.getContentPane().add(lblNewLabel);
+		
+		JScrollPane grilla = new JScrollPane();
+		grilla.setBounds(202, 11, 393, 217);
+		frame.getContentPane().add(grilla);
+		
+		JButton btnNewButton_2_1 = new JButton("Soluci\u00F3n 2");
+		//Action Solución 2
+		btnNewButton_2_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_2_1.setBounds(10, 159, 175, 29);
+		frame.getContentPane().add(btnNewButton_2_1);
+		
+		JButton btnNewButton_2_1_1 = new JButton("Soluci\u00F3n 3");
+		//Action Solución 2
+		btnNewButton_2_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_2_1_1.setBounds(10, 194, 175, 29);
+		frame.getContentPane().add(btnNewButton_2_1_1);
 	}
 }
