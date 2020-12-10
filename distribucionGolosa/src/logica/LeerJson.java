3c800b43908fc2af0a59f4c831387851b3e3641c
Usuario <Usuario@DESKTOP-SJR6E1A>
2020-12-09 21:20:12 -0300

    collection to array

--- distribucionGolosa/src/logica/LeerJson.java
+++ distribucionGolosa/src/logica/LeerJson.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,7 +16,8 @@ import com.google.gson.reflect.TypeToken;
 public class LeerJson {
 
 	public static void main(String[] args) {
-		String json= ""; 
+		String json= "";
+		Gson gson = new Gson(); 
 		try {
 			BufferedReader bc = new BufferedReader(new FileReader("../DistribucionGolosa/distribucionGolosa/src/logica/cliente.json"));
 			String linea= ""; 
@@ -31,14 +33,21 @@ public class LeerJson {
 			Logger.getLogger(LeerJson.class.getName()).log(Level.SEVERE, null, ex);
 		}
 		
-		System.out.println(json);
+		//System.out.println(json);
 		
-		Gson gson = new Gson(); 
+//		Gson gson = new Gson(); 
+//		Coordenada c = gson.fromJson(json, Coordenada.class); 
 		
 		Type collectionType = new TypeToken<Collection<Coordenada>>(){}.getType();
 		Collection<Coordenada> c = gson.fromJson(json, collectionType);
-//		Coordenada c = gson.fromJson(json, Coordenada.class); 
-		System.out.println(c);
+		ArrayList<Coordenada> newList = new ArrayList<>(c);
+		
+		for(Coordenada coordenada : newList) {
+			System.out.println("****"+coordenada+"*****");
+		}
+		
+		
+		//System.out.println(newList);
 	}
 
 }
