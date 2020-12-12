908723702af367065c83ee10376da83b4142c10b
sabri <sabri@DESKTOP-GRUEQNT>
2020-12-12 01:51:19 -0300

    refactorice código de interfaz

--- distribucionGolosa/src/interfaz/Interfaz.java
+++ distribucionGolosa/src/interfaz/Interfaz.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
+import java.awt.Button;
+
 import javax.swing.JTextField;
 
 import logica.Cliente;
@@ -28,6 +30,12 @@ public class Interfaz {
 
 	private JFrame frame;
 	private JPanel map; 
+	private JButton btnLeerClientes;
+	private JButton btnLeerPosiblesDistribuidoras;
+	private JButton btnsol1; 
+	private JButton btnsol2; 
+	private JButton btnsol3; 
+	
 	/**
 	 * Launch the application.
 	 */
@@ -63,33 +71,10 @@ public class Interfaz {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("Leer clientes");
-		//Action leer Clientes
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		btnNewButton.setBounds(10, 58, 175, 29);
-		frame.getContentPane().add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Leer posibles distribuidoras");
-		//Action leer Distribuidoras
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnNewButton_1.setBounds(10, 92, 175, 29);
-		frame.getContentPane().add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Solución 1");
-		//Action Solución 1
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnNewButton_2.setBounds(10, 126, 175, 29);
-		frame.getContentPane().add(btnNewButton_2);
+		leerClientes();
+		leerPosiblesDistribuidoras();
+		mostrarSolucion1();
+		mostrarSolucion2();	
 		
 		JLabel lblNewLabel = new JLabel("Distribuci\u00F3n Golosa");
 		lblNewLabel.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 18));
@@ -97,24 +82,7 @@ public class Interfaz {
 		lblNewLabel.setBounds(10, 11, 182, 36);
 		frame.getContentPane().add(lblNewLabel);
 		
-		JButton btnNewButton_2_1 = new JButton("Soluci\u00F3n 2");
-		//Action Solución 2
-		btnNewButton_2_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnNewButton_2_1.setBounds(10, 159, 175, 29);
-		frame.getContentPane().add(btnNewButton_2_1);
-		
-		JButton btnNewButton_2_1_1 = new JButton("Soluci\u00F3n 3");
-		//Action Solución 2
-		btnNewButton_2_1_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnNewButton_2_1_1.setBounds(10, 194, 175, 29);
-		frame.getContentPane().add(btnNewButton_2_1_1);
-		
+		//Creacion de panel para mapa 
 		JPanel Map = new JPanel();
 		Map.setBounds(205, 11, 305, 214);
 		frame.getContentPane().add(Map);
@@ -122,9 +90,53 @@ public class Interfaz {
 		map = new JMapViewer();
 		((JMapViewer) map).setZoomContolsVisible(true);
 		Coordinate coordinate = new Coordinate(-34.521, -58.7008);
-		((JMapViewer) map).setDisplayPosition(coordinate, 15);
+		((JMapViewer) map).setDisplayPosition(coordinate, 13);
 		
 		Map.add(map);
 
+	}
+	
+	private void leerClientes() {
+		btnLeerClientes = new JButton("Leer clientes");
+		//Action leer Clientes
+		btnLeerClientes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		btnLeerClientes.setBounds(10, 58, 175, 29);
+		frame.getContentPane().add(btnLeerClientes);
+	}
+	
+	private void leerPosiblesDistribuidoras() {
+		btnLeerPosiblesDistribuidoras = new JButton("Leer posibles distribuidoras");
+		//Action leer Distribuidoras
+		btnLeerPosiblesDistribuidoras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnLeerPosiblesDistribuidoras.setBounds(10, 92, 175, 29);
+		frame.getContentPane().add(btnLeerPosiblesDistribuidoras);
+	}
+
+	private void mostrarSolucion1() {
+		btnsol1 = new JButton("Solución 1");
+		//Action Solución 1
+		btnsol1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnsol1.setBounds(10, 126, 175, 29);
+		frame.getContentPane().add(btnsol1);
+	}
+	private void mostrarSolucion2() {
+		btnsol2 = new JButton("Soluci\u00F3n 2");
+		//Action Solución 2
+		btnsol2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnsol2.setBounds(10, 159, 175, 29);
+		frame.getContentPane().add(btnsol2);
 	}
 }
